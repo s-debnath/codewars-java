72b3d4e493410ca60ccebb76a9b12266e19828f7
Subhrajit Debnath <sdebnath@jahnelgroup.com>
2018-01-11 12:23:02 -0500

    Initialized kata problem
    https://www.codewars.com/kata/simple-assembler-interpreter/train/java

---
+++ src/main/java/io/bytewize/kata/kyu/five/SimpleAssembler.java
@@ -0,0 +1,12 @@
+package io.bytewize.kata.kyu.five;
+
+import java.util.Map;
+
+/**
+ * https://www.codewars.com/kata/simple-assembler-interpreter/train/java
+ */
+public class SimpleAssembler {
+    public static Map<String, Integer> interpret(String[] program){
+        return null;
+    }
+}
